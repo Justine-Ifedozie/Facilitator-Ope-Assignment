e506defdd72bed94ba0b1cbd0f29cfb07c7be8da
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-04-30 01:24:31 +0100

    Add files via upload

---
+++ SecondLargestNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SecondLargestNumber {
+	public static void main (String[] args) {
+		Scanner keyboardInput = new Scanner(System.in);
+
+		int number = 0, largest = 0, secondLargest = 0, counter;
+
+				for (counter = 0; counter != 10; counter++) {
+
+				System.out.print("Enter a number: ");
+				number = keyboardInput.nextInt();
+
+			}
+
+
+
+	}
+}
